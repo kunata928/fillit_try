8ea3c43ba5ca3c484f5b6ca2ee4c526f1d5edd72
kunata928 <kunata928@gmail.com>
2019-11-03 21:03:16 +0300

    some solve try

--- srcs/solve.c
+++ srcs/solve.c
@@ -12,39 +12,60 @@
 
 #include "../includes/fillit.h"
 
-static int	ft_sqrt(int num)
+static char	**ft_sqrt(int num, char **arr)
 {
 	int rt;
 	int nums;
+	int i;
 
 	nums = num * 4;
 	rt = 2;
-	while (rt * rt < num)
-		rt++;
-	return (rt);
-}
-
-static int	try_to_fill(t_tetris *tetris, char **arr)
-{
-	int		i;
-	t_xy	*xy;
-
 	i = 0;
-
-	while (i < 8)
+	while (rt * rt < nums)
+		rt++;
+	arr = (char **)ft_memalloc(sizeof(char *) * rt);
+	while (i < rt)
 	{
-		arr[xy->x + tetris->piece[2 * i]][xy->y + tetris->piece[2 * i + 1]] = '#';
+		arr[i] = (char *)ft_memalloc(sizeof(char) * 8);
 		i++;
 	}
-	while
+	return (arr);
+}
+
+static int	try_to_fill(t_tetris *tetris, char **arr, t_xy xy0)
+{
+	int		i;
+	t_xy	xy;
+
+	i = 0;
+	while (i < 4)
+	{
+		xy.x = xy0.x + tetris->piece[2 * i];
+		xy.y = xy0.y + tetris->piece[2 * i + 1];
+		if (!arr[xy.x][xy.y])
+			arr[xy.x][xy.y] = '#';
+		else
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 static int	fill(t_tetris **tetris, int nums)
 {
-	char	*arr;
+	char	**arr;
+	int i;
+	t_xy xy0;
 
-	arr = (char *)ft_memalloc(sizeof(char) * (ft_sqrt(nums)));
-
+	i = 0;
+	xy0.x = 0;
+	xy0.y = 0;
+	arr = ft_sqrt(nums, arr); // create 2dem array start size
+	while (i < nums)
+	{
+		try_to_fill(tetris[i], arr, xy0);
+		i++;
+	}
 }
 
 char		*solver(t_tetris **tetris)
